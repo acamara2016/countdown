f1b20fc8914f37dd6d4422752e1b42b684f8f951
Adama Camara <48602776+acamara2016@users.noreply.github.com>
2021-06-28 00:09:53 -0300

    busker coming soon

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var countDownDate = new Date("August 1, 2021 15:37:25").getTime();
+var countDownDate = new Date("July 31, 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
